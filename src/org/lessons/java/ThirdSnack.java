5ca626bad1285a8dc8ac144b86eb4c4a4b667c4d
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-16 15:13:27 +0200

    Third snack completed

--- src/org/lessons/java/ThirdSnack.java
+++ src/org/lessons/java/ThirdSnack.java
@@ -1,4 +1,22 @@
 package org.lessons.java;
 
 public class ThirdSnack {
+    /*
+     *  Snack3
+     *  Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+     */
+
+    public static void main(String[] args) {
+        int sum = 0;
+        
+        int[] numbers = {2, 3, 4, 6, 7, 10, 8};
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0){
+                sum += numbers[i];
+            }
+        }
+
+        System.out.println("Sum of odd numbers in the list is: "+ sum);
+    }
 }
